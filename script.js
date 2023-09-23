47d7827f2f6e3f487f3290e37430948d398cd4cc
SisiVero <105162163+SisiVero@users.noreply.github.com>
2023-09-23 18:05:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/"
+];
+let passwordButton = document.getElementById("passwordBtn");
+let passwordOne = document.getElementById("password1");
+let passwordTwo = document.getElementById("password2");
+
+passwordButton.addEventListener("click", function () {
+  passwordOne.textContent = generatePasswords();
+  passwordTwo.textContent = generatePasswords();
+});
+
+let passwordLength = 10;
+
+function generatePasswords() {
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters[randomNumber];
+  }
+  return password;
+}
